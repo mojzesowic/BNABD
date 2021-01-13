4451fa287daa735b9b48117f272e179d140b0ebf
Jakub Wisniewski <jakubwisn@gmail.com>
2021-01-13 19:42:28 +0100

    add get employee by id

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +17,6 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     //get all employees
-
     @GetMapping("/employees")
     public List<Employee> getAllEployees() {
         return employeeRepository.findAll();
@@ -25,5 +26,13 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    //get employee by id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist:" + id));
+        return ResponseEntity.ok(employee);
     }
 }
